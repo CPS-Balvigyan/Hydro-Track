d1550047764f0962a04bead9ed8c49e0f4470e34
CPS-Balvigyan <balvigyancps@gmail.com>
2024-11-14 16:09:31 +0530

    Second Commit
    html-->reactjs

--- hydrotrack/src/App.js
+++ hydrotrack/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logoImage from "./assets/logo.png"; // Import the image
+import checkImage from "./assets/check.png"; // Import the image
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="logo">
+          <img src={logoImage} alt="" />
+          <p>HYDRO-TRACK</p>
+        </div>
+        <div className="heading">
+          <h1>DASHBOARD</h1>
+        </div>
+      </div>
+      <div className="container">
+        <div className="item map" />
+        <div className="item water_flow">
+          <div className="card">
+            <p>WATER FLOW</p>
+          </div>
+        </div>
+        <div className="item water_level">
+          <div className="card">
+            <p>WATER LEVEL</p>
+          </div>
+        </div>
+        <div className="item status">
+          <div className="card">
+            <p>Motor and Valve Status</p>
+          </div>
+        </div>
+        <div className="item usage" />
+      </div>
+      <div className="footer">
+        <img src={checkImage} alt="" />
+        <p>All Systems Operating Normally</p>
+      </div>
+    </>
   );
 }
 
